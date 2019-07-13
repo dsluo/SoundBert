6592eb3b9834683e08189b309803a054ccb5633b
David Luo <mail@dsluo.me>
2019-07-13 02:17:31 -0400

    example settings

--- launcher.py
+++ launcher.py
@@ -1,20 +1,18 @@
 import asyncio
+import logging
 
 import click
 import toml
 
 from soundbert import SoundBert
 
-try:
-    import uvloop
+log = logging.getLogger(__name__)
 
-    asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
-except ImportError:
-    pass
 
 @click.group()
 def cli():
     pass
+
 
 @cli.command()
 @click.option('--config', 'config_path', default='./settings.toml', help='Path to config file.')
@@ -22,9 +20,22 @@ def run(config_path):
     with open(config_path, 'r') as f:
         config = toml.load(f)
 
+    log_level = config['logging']['level']
+    log_level = getattr(logging, log_level.upper(), None)
+    if not isinstance(log_level, int):
+        raise ValueError('Invalid log level')
+
     token = config['bot'].pop('token')
     bot = SoundBert(config)
     bot.run(token)
 
+
 if __name__ == '__main__':
+    try:
+        import uvloop
+
+        asyncio.set_event_loop_policy(uvloop.EventLoopPolicy())
+    except ImportError:
+        pass
+
     cli()
