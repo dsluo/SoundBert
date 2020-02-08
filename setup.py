8409d2099b4fe149f2761a8eb0619dffe2691927
David Luo <me@dsluo.dev>
2020-02-07 22:49:27 -0500

    update setup.py: jishaku & dependencies; version bump; python requirement

--- setup.py
+++ setup.py
@@ -5,10 +5,9 @@ with open('requirements.txt') as f:
 
 extras_require = {
     'jishaku': [
-        'braceexpand==0.1.3',
-        'humanize==0.5.1',
-        'import-expression==0.5.0',
-        'jishaku==1.16.4.160'
+        'braceexpand == 0.1.5',
+        'import-expression==1.0.0',
+        'jishaku == 1.17.1.181'
     ]
 }
 
@@ -19,14 +18,14 @@ setup(
     name='soundbert',
     author='dsluo',
     url='https://https://github.com/dsluo/SoundBert',
-    version='0.1.0',
+    version='0.2.0',
     description='A soundboard bot for Discord.',
     long_description=readme,
     long_description_content_type='text/markdown',
     include_package_data=True,
     install_requires=requirements,
     extras_require=extras_require,
-    python_requires='>=3.7',
+    python_requires='>=3.8',
     packages=find_packages(),
     entry_points={
         'console_scripts': [
