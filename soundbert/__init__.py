b7cc20ba862fb1850464407fc805370300721f0e
David Luo <me@dsluo.dev>
2020-02-07 23:04:47 -0500

    central main function for setup.py entry_points

--- soundbert/__init__.py
+++ soundbert/__init__.py
@@ -1,3 +1,21 @@
 import logging
+import sys
+
+from .config import Config
+from .soundbert import SoundBert
 
 log = logging.getLogger(__name__)
+
+def main():
+    logging.basicConfig(level=logging.WARNING)
+
+    config = Config.from_env()
+
+    log_level = getattr(logging, config.log_level)
+    if not isinstance(log_level, int):
+        log.critical('Invalid log level.')
+        sys.exit(1)
+    log.setLevel(log_level)
+
+    bot = SoundBert(config)
+    bot.run()
--- soundbert/__main__.py
+++ soundbert/__main__.py
@@ -1,21 +1,4 @@
-import logging
-import sys
-
-from .config import Config
-from .soundbert import SoundBert
-
-logging.basicConfig(level=logging.WARNING)
-
-from . import log
+from . import main
 
 if __name__ == '__main__':
-    config = Config.from_env()
-
-    log_level = getattr(logging, config.log_level)
-    if not isinstance(log_level, int):
-        log.critical('Invalid log level.')
-        sys.exit(1)
-    log.setLevel(log_level)
-
-    bot = SoundBert(config)
-    bot.run()
+    main()
